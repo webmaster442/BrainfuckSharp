46169182c4170895a34aba8545e0c0ab9a1a934d
webmaster442 <ruzsinszki.gabor@gmail.com>
2022-07-25 19:34:49 +0200

    Compiler: Initial version

---
+++ BrainFuckSharp/CompilerOptions.cs
@@ -0,0 +1,18 @@
+﻿namespace BrainFuckSharp
+{
+    internal sealed record class CompilerOptions
+    {
+        public string Namespace { get; init; }
+        public int MemoryLimit { get; init; }
+        public string BrainFuckSource { get; init; }
+        public string OutputFileName { get; init; }
+
+        public CompilerOptions()
+        {
+            Namespace = "BrainFuck";
+            MemoryLimit = 4096;
+            BrainFuckSource = string.Empty;
+            OutputFileName = string.Empty;
+        }
+    }
+}
