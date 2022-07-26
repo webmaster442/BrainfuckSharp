92e73e2f261b8548aea6b73e64d6a89ca4533567
webmaster442 <ruzsinszki.gabor@gmail.com>
2022-07-26 18:17:45 +0200

    Removed csharp code generator

--- BrainfuckSharp.Tests/Usings.cs
+++ BrainfuckSharp.Tests/Usings.cs
@@ -1,6 +1,5 @@
 global using BrainFuckSharp.Lib;
 global using BrainFuckSharp.Lib.Domain;
 global using BrainFuckSharp.Lib.Internals;
-global using BrainFuckSharp.Tests.Properties;
 
 global using NUnit.Framework;
